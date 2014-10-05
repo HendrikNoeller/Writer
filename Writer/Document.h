7cfec4b6098e10a7733891b48452b82742c2dbbb
Hendrik Noeller <noeller@me.com>
2014-10-05 20:46:13 +0200

    Initial Commit

---
+++ Writer/Document.h
@@ -0,0 +1,15 @@
+//
+//  Document.h
+//  Writer
+//
+//  Created by Hendrik Noeller on 05.10.14.
+//  Copyright (c) 2014 Hendrik Noeller. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+
+@end
+
---
+++ Writer/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Writer
+//
+//  Created by Hendrik Noeller on 05.10.14.
+//  Copyright (c) 2014 Hendrik Noeller. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
