8cf7feb92be6402e1362207654f3291102d9fe64
Hendrik Noeller <noeller@me.com>
2016-05-12 08:29:24 +0200

    Readded outline view to fix weird problems with the scrolling. Changed scene number look in outline view again

--- Writer/Document.m
+++ Writer/Document.m
@@ -1209,7 +1209,7 @@ static NSString *forceLyricsSymbol = @"~";
             string = [string stringByReplacingOccurrencesOfString:@"EXT/INT" withString:@"I/E"];
             string = [string stringByReplacingOccurrencesOfString:@"EXT./INT" withString:@"I/E"];
             if (line.sceneNumber) {
-                return [NSString stringWithFormat:@"    #%@# %@", line.sceneNumber, [string stringByReplacingOccurrencesOfString:[NSString stringWithFormat:@"#%@#", line.sceneNumber] withString:@""]];
+                return [NSString stringWithFormat:@"    %@: %@", line.sceneNumber, [string stringByReplacingOccurrencesOfString:[NSString stringWithFormat:@"#%@#", line.sceneNumber] withString:@""]];
             } else {
                 return [@"    " stringByAppendingString:string];
             }
